22107b9157608a41d947c2219ad29aec9c2e83d1
lodowa255708 <255708@edu.p.lodz.pl>
2026-04-07 23:18:04 +0200

    Stale i annotacje

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,8 +2,12 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
-    private int total = -1;
+    public static final int TAB = 12;
+    public static final int TOTAL1 = -1;
+    public static final int TOTAL11 = 11;
+    public static final int RET = -1;
+    private final int[] numbers = new int[TAB];
+    private int total = TOTAL1;
 
     public int getTotal() {
         return total;
@@ -19,22 +23,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL1;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == TOTAL11;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return RET;
         return numbers[total--];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return RET;
         return numbers[total--];
     }
 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,12 +2,14 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    private int totalRejected = 0;
+    public static final int INT = 0;
+    private int totalRejected = INT;
 
     public int reportRejected() {
         return totalRejected;
     }
 
+    @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo()) {
             totalRejected++;
