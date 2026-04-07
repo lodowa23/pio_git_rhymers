2df2b9727bf0e2e425aa903a5b7b9a98db27eb62
Lodowa23 <pszewczyk036@gmail.com>
2026-04-07 20:33:26 +0200

    Getters and setters

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -1,9 +1,30 @@
 package edu.kis.vh.nursery.list;
 
-public class Node {
+class Node {
 
-    public int value;
-    public Node prev, next;
+    public int getValue() {
+        return value;
+    }
+
+    private int value;
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    private Node prev, next;
 
     public Node(int i) {
         value = i;
